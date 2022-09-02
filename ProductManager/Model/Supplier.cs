db9d07799890836aeaf6bea6d94b6909b2456e96
Gilford0306 <Gilford0306@gmail.com>
2022-09-02 13:55:19 +0300

    fix structure

---
+++ ProductManager/Model/Supplier.cs
@@ -0,0 +1,28 @@
+﻿namespace ProductManager.Model
+{
+    internal class Supplier
+    {
+        public int Id { get; set; }
+        public string NameSupplier { get; set; }
+
+
+        public Supplier()
+        {
+            Id = 0;
+            NameSupplier = string.Empty;
+
+        }
+        public Supplier(int id, string name)
+        {
+            Id = id;
+            NameSupplier = name;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}.Supplier  - {NameSupplier}";
+        }
+
+    }
+
+}
--- ProductManager/Program.cs
+++ ProductManager/Program.cs
@@ -13,21 +13,21 @@ do
     ModelSeting defaultconnect = JsonSerializer.Deserialize<ModelSeting>(File.ReadAllText(@"D:\Projects\ProductManager\ProductManager\Connection_String\setting.json"));
     string mybase = defaultconnect.Database.ToString();
     Console.WriteLine("\tProducts");
-    Console.WriteLine("All Products - press 1");
-    Console.WriteLine("Add Product - press 2");
-    Console.WriteLine("Edit Product - press 3");
-    Console.WriteLine("Deleted Product - press 4");
+    Console.WriteLine("All Supplires - press 1");
+    Console.WriteLine("Add Supplires  - press 2");
+    Console.WriteLine("Edit Supplires  - press 3");
+    Console.WriteLine("Deleted Supplires  - press 4");
     int k = int.Parse(Console.ReadLine());
     switch (k)
     {
         case 1:
             connect.Open();
             LogerCreate.Log($"Open datbase - {mybase}", LogLevel.Information);
-            var produts = connect.Query<Product>("SELECT * FROM [Product];");
-            produts.ToList().ForEach(Console.WriteLine);
-            if (produts.Count() == 0)
-                Console.WriteLine("No Products yet");
-            LogerCreate.Log($"Get items {produts.Count()} from database - {mybase} ", LogLevel.Information);
+            var supl = connect.Query<Supplier>("SELECT * FROM [Supplier];");
+            supl.ToList().ForEach(Console.WriteLine);
+            if (supl.Count() == 0)
+                Console.WriteLine("No Supplires yet");
+            LogerCreate.Log($"Get Supplir {supl.Count()} from database - {mybase} ", LogLevel.Information);
             connect.Close();
             LogerCreate.Log($"Close datbase {mybase}", LogLevel.Information);
 
@@ -35,17 +35,13 @@ do
         case 2:
             Console.WriteLine("Input name - ");
             string name = Console.ReadLine();
-            Console.WriteLine("Input CategoryId - ");
-            int cat = int.Parse(Console.ReadLine());
-            Console.WriteLine("Input SupplierId - ");
-            int sup = int.Parse(Console.ReadLine());
             connect.Open();
             LogerCreate.Log($"Open datbase {mybase}", LogLevel.Information);
-            Product product = new Product(-1, name, cat, sup);
-            int rows = connect.Execute($"INSERT INTO [dbo].[Product]([Name],[CategoryId],[SupplierId])VALUES (\'{product.Name}\', {product.CategoryId},\'{product.SupplierId}\');");
+            Supplier supplier = new Supplier (-1, name);
+            int rows = connect.Execute($"INSERT INTO [dbo].[Supplier]([NameSupplier])VALUES (\'{supplier.NameSupplier}\');");
             if (rows > 0)
             {
-                Console.WriteLine($"Product added!");
+                Console.WriteLine($" Supplier added!");
                 LogerCreate.Log($"INSERT {name} to datbase {mybase}", LogLevel.Information);
             }
             connect.Close();
@@ -54,21 +50,17 @@ do
         case 3:
             connect.Open();
             LogerCreate.Log($"Open datbase", LogLevel.Information);
-            var items = connect.Query<Product>("SELECT * FROM [Product];");
+            var items = connect.Query<Supplier>("SELECT * FROM [Supplier];");
             items.ToList().ForEach(Console.WriteLine);
             Console.WriteLine("Input Id - ");
             int id = int.Parse(Console.ReadLine());
             Console.WriteLine("Input name - ");
             name = Console.ReadLine();
-            Console.WriteLine("Input CategoryId- ");
-            int categoryId = int.Parse(Console.ReadLine());
-            Console.WriteLine("Input SupplierId - ");
-            int supplierId = int.Parse(Console.ReadLine());
-            rows = connect.Execute($"UPDATE Product SET [Name]='{name}',[CategoryId]='{categoryId}',[SupplierId]='{supplierId}'WHERE id ={id}");
+            rows = connect.Execute($"UPDATE Supplier SET [NameSupplier]='{name}'WHERE id ={id}");
             if (rows > 0)
             {
                 Console.WriteLine("Edited");
-                LogerCreate.Log($"UPDATE Product id = {id} in database - {mybase}", LogLevel.Information);
+                LogerCreate.Log($"UPDATE Supplier id = {id} in database - {mybase}", LogLevel.Information);
             }
             else
             {
@@ -81,15 +73,15 @@ do
         case 4:
             connect.Open();
             LogerCreate.Log($"Open datbase - {mybase}", LogLevel.Information);
-            items = connect.Query<Product>("SELECT * FROM [Product];");
+            items = connect.Query<Supplier>("SELECT * FROM [Supplier];");
             items.ToList().ForEach(Console.WriteLine);
             Console.WriteLine("Input Id - ");
             id = int.Parse(Console.ReadLine());
-            rows = connect.Execute($"DELETE FROM [dbo].[Product] WHERE Id = {id}");
+            rows = connect.Execute($"DELETE FROM [dbo].[Supplier] WHERE Id = {id}");
             if (rows > 0)
             {
                 Console.WriteLine("Row is deleted");
-                LogerCreate.Log($"item id {id} deleted in database - {mybase}", LogLevel.Information);
+                LogerCreate.Log($"Supplier id {id} deleted in database - {mybase}", LogLevel.Information);
             }
             else
             {
@@ -99,9 +91,6 @@ do
             connect.Close();
             LogerCreate.Log($"Close datbase - {mybase}", LogLevel.Information);
             break;
-        //        //    case 5:
-        //        //        Analysis.Show();
-        //        //        break;
         default:
             Console.WriteLine("Only Number 1 - 4");
             break;
